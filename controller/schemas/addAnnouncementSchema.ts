f0e49f90c825876ce1bdfc61a4cebf5b27ccaf13
dim <dimitri86120@outlook.com>
2025-09-01 22:37:49 +0200

    mise en place d'un schema Zod pour le formulaire

--- controller/schemas/addAnnouncementSchema.ts
+++ controller/schemas/addAnnouncementSchema.ts
@@ -21,18 +21,3 @@ export const announcementSchema = z.object({
     salary_unit: z.enum(["heure", "jour", "mois", "an"]),
     password: z.string().min(6, "Mot de passe trop court"),
 });
-
-// import { z } from "zod";
-
-// export const announcementSchema = z.object({
-//   title: z.string().min(3, "Le titre doit contenir au moins 3 caractères"),
-//   description: z.string().min(10, "La description est trop courte"),
-//   skills: z.string().transform((val) => val.split(",")),
-//   mission_type: z.enum(["CDI", "CDD", "Freelance"], {
-//     required_error: "Le type de mission est obligatoire",
-//   }),
-//   start_date: z.string().optional(), // tu peux utiliser z.date() si tu transformes avant
-//   salary: z.string().regex(/^\d+$/, "Le salaire doit être un nombre"),
-//   salary_unit: z.enum(["heure", "jour", "mois", "an"]),
-//   password: z.string().min(6, "Mot de passe trop court"),
-// });
